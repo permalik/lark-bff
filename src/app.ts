a29fa7b1c0466569c037258daaac2ce2a6fd0fe8
permalik <permalik@protonmail.com>
2025-07-29 06:11:28 -0500

    feat: impl cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import cors from "cors";
 import promptRoutes from "./routes/promptRoutes";
 import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: false,
+  }),
+);
 app.use(express.json());
 app.use("/api/prompts", promptRoutes);
 app.use(errorHandler);
